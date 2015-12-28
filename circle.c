3826c42fb780dfa623c19fdf0c9506ca6323b347
tmslive24 <themrinalsinha@yahoo.com>
2015-12-28 23:58:25 +0530

    Update circle.c

--- circle.c
+++ circle.c
@@ -3,7 +3,7 @@ int main()
 {
   int gd = DETECT, gm = 0;
   initgraph(&gd, &gm,NULL); // initialize graphics
-  circle(300, 300, 150); // graphics function for circle
+  circle(300, 300, 200); // graphics function for circle
   getch(); // this is must to hold output!
   closegraph();
   return 0;
